95032c87df7f8976aca0325e9804f7dae2c90287
Léo Moraes <leo.herte@mercadolivre.com>
2025-02-25 03:15:43 -0300

    Return basic data of pokemon
    Using /pokedex/{number or name}
    Return {id and name}
    Without filter, this is a POC

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
+use Illuminate\Support\Facades\Http;
 
 Route::get('/user', function (Request $request) {
     $users = User::all();
     return $users;
 }); //->middleware('auth:sanctum');
+
+Route::get('/pokedex/{id}', function (Request $request) {
+    $pokemon = $request->id;
+    $resource = Http::get('https://pokeapi.co/api/v2/ability/'.$pokemon);
+    $output = [$resource['id'], $resource['name']];
+    return response()->json($output);
+});
