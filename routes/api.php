38ce903702da3d28ffe609258f93f38ed3a136be
Léo Moraes <leo.herte@mercadolivre.com>
2025-02-25 03:23:31 -0300

    fix route name and add filter

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
-Route::get('/user', function (Request $request) {
+Route::get('/users', function (Request $request) {
     $users = User::all();
+    return $users;
+}); //->middleware('auth:sanctum');
+
+Route::get('/users/{id}', function (Request $request, $id) {
+    $users = User::find($id);
     return $users;
 }); //->middleware('auth:sanctum');
