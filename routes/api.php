40a1c0699faa5f21891734f8e4f87843fe14a917
Léo Moraes <leo.herte@mercadolivre.com>
2025-02-25 03:05:22 -0300

    API - list all users
    Without restriction (Sanctum)
    WIP

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    $users = User::all();
+    return $users;
+}); //->middleware('auth:sanctum');
